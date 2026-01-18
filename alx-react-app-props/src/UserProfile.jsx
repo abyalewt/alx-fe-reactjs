8d98a5248ed287a5eb98e3f9a1d132f8ac1e7c4b
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-18 22:02:31 +0300

    Corrected component chain to use UserProfile

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,7 +1,16 @@
-import UserProfile from './UserProfile';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
-function ProfilePage() {
-    return <UserProfile />;
+function UserProfile() {
+    // Use the useContext hook to get the data
+    const userData = useContext(UserContext);
+
+    return (
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2>{userData.name}</h2>
+            <p>Email: {userData.email}</p>
+        </div>
+    );
 }
 
-export default ProfilePage;
+export default UserProfile;
