5f8a8c0c3443dd6c71f304f2348aad3148f38d0f
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-18 21:34:52 +0300

    user context updated

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 
+// Create a new Context object
 const UserContext = createContext();
 
+// Export it so other components can use it
 export default UserContext;
